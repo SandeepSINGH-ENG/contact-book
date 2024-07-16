4f5826fc7d61a140c88331a9824a707d613a20a5
SandeepSINGH-ENG <marwahasingh503@gmail.com>
2024-07-16 17:43:17 -0400

    Initial commit

---
+++ contact-book/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <div class="main-box">
+    <nav>
+      <ul>
+       <li><img src="/vite.svg"</li>
+        <li><router-link to="/">CONTACT BOOK <img src="./assets/book.svg"></router-link></li>
+        <li><router-link to="/add">ADD NEW<img src="./assets/add.svg"></router-link></li>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ contact-book/src/components/ContactItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <li>
+      <span>
+        <span class="contact-names">{{ contact.firstName }} {{ contact.lastName }}</span><br> {{ contact.email }}</span>
+      <span class="controls">
+        <button @click="editContact"><img src="../assets/edit.svg"></button>
+      <button @click="deleteContact"><img src="../assets/delete.svg"></button>
+      </span>
+      
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      contact: Object,
+    },
+    methods: {
+      editContact() {
+        this.$emit('editContact', this.contact)
+      },
+      deleteContact() {
+        this.$emit('deleteContact', this.contact)
+      },
+    },
+  }
+  </script>
+  
